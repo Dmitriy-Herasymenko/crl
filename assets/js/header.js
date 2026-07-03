8ae939ecef05d25c83a00068ff7116222b236abb
Dmitriy-Herasymenko <shiningxors@gmail.com>
2026-07-03 14:19:43 +0300

    Add news list/detail pages, fix nav dropdown hover gap
    Static checkpoint before migrating the site to Next.js.

--- assets/js/header.js
+++ assets/js/header.js
@@ -169,7 +169,7 @@
           </button>
           <div class="dropdown" role="menu">
             <div class="dropdown-section">
-              <a href="https://uman.crl.net.ua/category/novyny/" target="_blank" class="dropdown-item" role="menuitem">
+              <a href="novyny.html" class="dropdown-item" role="menuitem">
                 <span class="dropdown-icon"><svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"/></svg></span>
                 Новини
               </a>
@@ -241,7 +241,7 @@
       </div>
       <div class="mob-section">
         <p class="mob-label">Публікації</p>
-        <a href="https://uman.crl.net.ua/category/novyny/"                       target="_blank" class="mob-item">Новини</a>
+        <a href="novyny.html"                                                    class="mob-item">Новини</a>
         <a href="https://uman.crl.net.ua/category/porady-likarya/"               target="_blank" class="mob-item">Поради лікаря</a>
         <a href="https://uman.crl.net.ua/category/vaktsynatsiya-vid-covid-19/"   target="_blank" class="mob-item">Вакцинація</a>
       </div>
